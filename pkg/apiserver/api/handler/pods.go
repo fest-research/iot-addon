cfd4127262f4a0eb76b50d088edb93ba9a2f1eac
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2017-01-18 15:36:39 +0100

    Remove unused code

--- pkg/apiserver/api/handler/pods.go
+++ pkg/apiserver/api/handler/pods.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"net/http"
-	"time"
 
 	"k8s.io/client-go/kubernetes"
 
@@ -11,29 +10,6 @@ import (
 	"github.com/fest-research/IoT-apiserver/pkg/apiserver/proxy"
 	"github.com/fest-research/IoT-apiserver/pkg/apiserver/watch"
 )
-
-// nothing will ever be sent down this channel
-var neverExitWatch <-chan time.Time = make(chan time.Time)
-
-// timeoutFactory abstracts watch timeout logic for testing
-type TimeoutFactory interface {
-	TimeoutCh() (<-chan time.Time, func() bool)
-}
-
-// realTimeoutFactory implements timeoutFactory
-type realTimeoutFactory struct {
-	timeout time.Duration
-}
-
-// TimeoutChan returns a channel which will receive something when the watch times out,
-// and a cleanup function to call when this happens.
-func (w *realTimeoutFactory) TimeoutCh() (<-chan time.Time, func() bool) {
-	if w.timeout == 0 {
-		return neverExitWatch, func() bool { return false }
-	}
-	t := time.NewTimer(w.timeout)
-	return t.C, t.Stop
-}
 
 type PodService struct {
 	clientSet     *kubernetes.Clientset
