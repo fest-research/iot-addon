cc8314137fb3e8cad0c79018865d3558ef9757a6
Christoph Held <christoph.held@est.fujitsu.com>
2017-01-19 12:50:06 +0100

    added event endpoint

--- pkg/apiserver/api/handler/factory.go
+++ pkg/apiserver/api/handler/factory.go
@@ -35,6 +35,9 @@ func (this *ServiceFactory) init() {
 	// Pod service
 	this.registerService(NewPodService(this.proxy, controller.NewPodController()))
 
+	// Node service
+	this.registerService(NewEventService(this.proxy))
+
 	// Kubernetes service
 	this.registerService(NewKubeService(this.proxy))
 }
