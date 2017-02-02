2f6335aae9ffe1b89a7993a7b69d99f57ce300ff
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-02-02 11:06:08 +0100

    Refactor iotpods.go

--- pkg/kubernetes/iotpod.go
+++ pkg/kubernetes/iotpod.go
@@ -12,13 +12,16 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-// CreateDaemonSetPod created ds pod on specific device
+// CreateDaemonSetPod creates IotPod for IotDaemonSet on specific IotDevice.
 func CreateDaemonSetPod(ds types.IotDaemonSet, device types.IotDevice, restClient *rest.RESTClient) error {
+	log.Printf("Trying to create IotPods for %s %s\n", ds.Metadata.SelfLink, ds.TypeMeta.Kind)
+
 	labelsMap := map[string]string{
 		types.CreatedBy:      types.IotDaemonSetType + "." + ds.Metadata.Name,
 		types.DeviceSelector: device.Metadata.Name,
 	}
 	common.MapCopy(labelsMap, ds.Spec.Template.ObjectMeta.Labels)
+
 	return restClient.Post().
 		Namespace(ds.Metadata.Namespace).
 		Resource(types.IotPodType).
@@ -33,7 +36,9 @@ func CreateDaemonSetPod(ds types.IotDaemonSet, device types.IotDevice, restClien
 				Labels:    labelsMap,
 			},
 			Spec: ds.Spec.Template.Spec,
-		}).Do().Error()
+		}).
+		Do().
+		Error()
 }
 
 // isPodCorrectlyScheduled checks if pod is correctly scheduled.
@@ -46,7 +51,7 @@ func IsPodCorrectlyScheduled(ds types.IotDaemonSet, pod types.IotPod) bool {
 	}
 }
 
-// getDevicesMissingPods filters daemon set destined devices and returns devices without any existing pods.
+// GetDevicesMissingPods filters daemon set destined devices and returns devices without any existing pods.
 func GetDevicesMissingPods(dsDestinedDevices []types.IotDevice, existingPods []types.IotPod) []types.IotDevice {
 	var devicesMissingPod []types.IotDevice
 	for _, device := range dsDestinedDevices {
@@ -96,14 +101,18 @@ func DeletePod(restClient *rest.RESTClient, pod types.IotPod) error {
 		Error()
 }
 
+// UpdatePod updates specific IotPod spec and labels.
 func UpdatePod(restClient *rest.RESTClient, pod types.IotPod, template v1.PodTemplateSpec) error {
+	log.Printf("Trying to update %s %s\n", pod.Metadata.SelfLink, pod.TypeMeta.Kind)
+
+	// Update IotPod spec.
 	pod.Spec = template.Spec
 
+	// Update IotPod labels merging with old ones.
 	labelsMap := map[string]string{
 		types.CreatedBy:      pod.Metadata.Labels[types.CreatedBy],
 		types.DeviceSelector: pod.Metadata.Labels[types.DeviceSelector],
 	}
-
 	common.MapCopy(labelsMap, template.ObjectMeta.Labels)
 	pod.Metadata.Labels = labelsMap
 
@@ -120,8 +129,7 @@ func UpdatePod(restClient *rest.RESTClient, pod types.IotPod, template v1.PodTem
 func IsPodCreated(restClient *rest.RESTClient, ds types.IotDaemonSet, device types.IotDevice) bool {
 	var podList types.IotPodList
 
-	// Ignores error.
-	restClient.Get().
+	err := restClient.Get().
 		Resource(types.IotPodType).
 		Namespace(ds.Metadata.Namespace).
 		LabelsSelectorParam(labels.Set{
@@ -130,6 +138,9 @@ func IsPodCreated(restClient *rest.RESTClient, ds types.IotDaemonSet, device typ
 		}.AsSelector()).
 		Do().
 		Into(&podList)
+	if err {
+		return false
+	}
 
 	return len(podList.Items) > 0
 }
@@ -139,7 +150,6 @@ func GetPodDevice(restClient *rest.RESTClient, pod types.IotPod) (types.IotDevic
 	var device types.IotDevice
 
 	fieldSelector, err := fields.ParseSelector("metadata.name=" + pod.Metadata.Labels[types.DeviceSelector])
-
 	if err != nil {
 		return device, err
 	}
