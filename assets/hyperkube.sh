ec7b54c04d4eab66241df564ef2503a54d8b39d5
Christoph Held <christoph.held@est.fujitsu.com>
2017-02-24 11:35:35 +0100

    added hyperkube command

---
+++ assets/hyperkube.sh
@@ -0,0 +1,20 @@
+K8S_VERSION="v1.5.3"
+
+docker run \
+    --volume=/sys:/sys:ro \
+    --volume=/dev:/dev \
+    --volume=/var/lib/docker/:/var/lib/docker:ro \
+    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw,shared \
+    --volume=/var/run:/var/run:rw \
+    --net=host \
+    --pid=host \
+    --privileged=true \
+    -d \
+    gcr.io/google_containers/hyperkube-amd64:${K8S_VERSION} \
+    /hyperkube kubelet \
+        --allow-privileged=true \
+        --hostname-override="127.0.0.1" \
+        --address="0.0.0.0" \
+        --api-servers=http://localhost:8080 \
+        --config=/etc/kubernetes/manifests \
+        --v=2
